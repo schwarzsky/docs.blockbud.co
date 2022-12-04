78c4d4b43ef9ec96e5c7d73fd1f6620b5e70d155
schwarzsky <bkoray@protonmail.com>
2022-12-04 18:21:50 +0300

    yoy!

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,33 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Blockbud</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Blockbud/docs.blockbud.co.git',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://blockbud.co/discord',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Blockbud/docs.blockbud.co.git',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Blockbud',
   },
+  head: (
+    <>
+      <script
+  lang="javascript"
+  dangerouslySetInnerHTML={{
+    __html: `if (!window.localStorage.getItem("theme_default")) {
+      window.localStorage.setItem("theme", "dark");
+      window.localStorage.setItem("theme_default", "dark");
+      document.documentElement.classList.add("dark");
+      document.documentElement.classList.remove("light");
+    }`,
+  }}
+/>;
+    </>
+  )
 }
+
 
 export default config
